87ac8cce78c2ed53db0905abe61119ab9f170be9
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2022-08-18 13:24:11 -0500

    Add coverage for factor()

--- tests/main_test.py
+++ tests/main_test.py
@@ -6,8 +6,14 @@ import pytest
 import bftools
 
 tests = [
-    "".join(random.choice(string.ascii_letters) for _ in range(10)) for _ in range(1000)
+    "".join(random.choice(string.ascii_letters) for _ in range(10)) for _ in range(100)
 ]
+
+numbers = (
+    [random.randint(1, 10000) for _ in range(5)]  # Random numbers
+    + [random.randint(1, 100) ** 2 for _ in range(5)]  # Squares
+    + [0]
+)
 
 
 @pytest.fixture
@@ -19,3 +25,9 @@ def compiler():
 @pytest.mark.parametrize("code", tests)
 def test_conversions(compiler, code):
     assert compiler and code  # TODO: make tests
+
+
+@pytest.mark.parametrize("num", numbers)
+def test_factor(num):
+    a, b = bftools.factor(num)
+    assert a * b == num
